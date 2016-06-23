507eddec5ffcdeed23dbb8b138cfb30aa191857b
Uladox <uladoxiental@gmail.com>
2016-06-22 21:27:20 -1000

    Adds function for getting strlit string. 📞

--- strlit_parser.c
+++ strlit_parser.c
@@ -1,4 +1,4 @@
-#include <stddef.h>
+#include <stdlib.h>
 
 #include "core.h"
 #include "text_utils.h"
@@ -77,4 +77,83 @@ out:
 	token->len = curr - token->dat.text;
 	info->cue.text->lines += line;
 	return parsed;
+}
+
+static inline char
+backslash_curr(char curr)
+{
+	static const char vals[] = {
+		['a'] = '\a',
+		['b'] = '\b',
+		['f'] = '\f',
+		['n'] = '\n',
+		['r'] = '\r',
+		['t'] = '\t',
+		['v'] = '\v',
+		['\''] = '\''
+	};
+
+	return vals[(int) curr];
+
+}
+
+enum spar_parsed
+spar_strlit_str(struct spar_token *token, char **str_ref)
+{
+	int backslashed = 0;
+	const char *curr = token->dat.text;
+	char *str = malloc(token->data_size);
+	char *begin = str;
+
+	while (1) {
+		switch (*++curr) {
+		case '\0':
+			goto error;
+		case '\\':
+			if (backslashed) {
+				*str = '\\';
+				++str;
+			}
+
+		        backslashed = !backslashed;
+			break;
+		case '\"':
+			if (backslashed) {
+				backslashed = 0;
+				*str = '\"';
+				++str;
+				break;
+			}
+
+			goto out;
+		case 'a':
+		case 'b':
+		case 'f':
+		case 'n':
+		case 'r':
+		case 't':
+		case 'v':
+		case '\'':
+			*str = (backslashed) ? backslash_curr(*curr) : *curr;
+			backslashed = 0;
+			++str;
+			break;
+		default:
+		        if (backslashed)
+				goto error;
+
+			*str = *curr;
+			++str;
+			break;
+		}
+	}
+error:
+	free(begin);
+	*str_ref = NULL;
+	return SPAR_ERROR;
+out:
+	*str = '\0';
+	*str_ref = begin;
+	return SPAR_OK;
+
 }
--- strlit_parser.h
+++ strlit_parser.h
@@ -5,3 +5,6 @@
 
 extern const char spar_type_strlit[];
 extern struct spar_parser spar_strlit_parser;
+
+enum spar_parsed
+spar_strlit_str(struct spar_token *token, char **str_ref);
